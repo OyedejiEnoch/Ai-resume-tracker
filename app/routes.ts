b3fed4b794197ae8dceb8942c1f4f9ffd01d626d
Enoch <oyedejienoch@gmail.com>
2025-08-20 10:16:30 +0100

    update routes

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,5 @@ export default [index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
     route('/upload', 'routes/upload.tsx'),
     route('/resume/:id', 'routes/resume.tsx'),
-    route('/wipe', 'routes/wipe.tsx'),
+    // route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
